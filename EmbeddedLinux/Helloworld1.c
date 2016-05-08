abad1c12c157287c62e313c99488ee1b52876cc3
Carlos E. Davaloc P <dcarlosedp@yahoo.com.mx>
2016-05-08 16:03:20 +0000

    Adding new file
    Practice creating module kernel file
    
    Signed-off-by: Carlos E. Davaloc P <dcarlosedp@yahoo.com.mx>

---
+++ EmbeddedLinux/Helloworld1.c
@@ -0,0 +1,21 @@
+#include <linux/init.h>
+#include <linux/kernel.h>
+#include <linux/module.h>
+
+static int module_init_function(void)
+{
+    printk(KERN_INFO "Module? Hello KARL!\n");
+    return 0;
+}
+
+static void module_exit_function(void)
+{
+    printk(KERN_INFO "Module? Bye KARL!\n");
+}
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("xe1gyq");
+MODULE_DESCRIPTION("My First Linux Kernel Module");
+
+module_init(module_init_function);
+module_exit(module_exit_function);
